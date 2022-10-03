5af4a04ff9045d948e05115be3d30d9b013f0eca
JackieChaaan <abelcvarghese12097@gmail.com>
2022-10-03 19:45:04 +0530

    update build for gh-pages

--- src/components/header/NavLink.js
+++ src/components/header/NavLink.js
@@ -6,7 +6,7 @@ function NavLink() {
     return (
         <>
             <ul>
-                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/ACVPortfolio'>Home</Link></li>
                 <li><Link to='/about'>About</Link></li>
                 <li><Link to='/portfoliopage'>Portfolio</Link></li>
                 <li><Link to='/contact'>Contact</Link> </li>
