535a130bf2d0866b176e29e10067154bff8736fb
JackieChaaan <abelcvarghese12097@gmail.com>
2023-05-19 12:48:50 +0530

    Update mobile navlink

--- src/components/header/MobileNavLinks.js
+++ src/components/header/MobileNavLinks.js
@@ -7,10 +7,10 @@ function MobileNavLink({navCloseHandler}) {
     <div className='mobileNav'>
        <span className='close h1' onClick={navCloseHandler}><i class="bi bi-x-circle"></i></span>
       <ul>
-        <li><Link to='/ACVPortfolio/'>Home</Link></li>
-        <li><Link to='/ACVPortfolio/about'>About</Link></li>
-        <li><Link to='/ACVPortfolio/portfoliopage'>Portfolio</Link></li>
-        <li><Link to='/ACVPortfolio/contact'>Contact</Link> </li>
+        <li><Link to='/ACVPortfolio/' onClick={navCloseHandler}>Home</Link></li>
+        <li><Link to='/ACVPortfolio/about' onClick={navCloseHandler}>About</Link></li>
+        <li><Link to='/ACVPortfolio/portfoliopage' onClick={navCloseHandler}>Portfolio</Link></li>
+        <li><Link to='/ACVPortfolio/contact' onClick={navCloseHandler}>Contact</Link> </li>
       </ul>
     </div>
   )
